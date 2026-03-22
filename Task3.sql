80ce5a59cdd258a62d4af5853f6743e715d95111
Pedro Leal <pedro.n.leal@tecnico.ulisboa.pt>
2026-03-22 16:17:09 +0000

    Add drop index instructions to task 3

--- Task3.sql
+++ Task3.sql
@@ -9,3 +9,9 @@ CREATE NONCLUSTERED INDEX IDX_Filtered_London_NewProperties
 ON ShortPricePaidData2025 (locality)
 INCLUDE (price)
 WHERE town = 'London' AND Old_New = 'Y';
+
+GO
+
+DROP INDEX IDX_New_town ON ShortPricePaidData2025;
+DROP INDEX IDX_Covering ON ShortPricePaidData2025;
+DROP INDEX IDX_Filtered_London_NewProperties ON ShortPricePaidData2025;
